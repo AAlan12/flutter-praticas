89744abff703e020e8ebfdf6e3b0d9ef33a5e463
Antonio Alan <tonio.alan07@gmail.com>
2023-03-06 10:15:46 -0300

    Criação de Bloco de atividades

--- lib/main.dart
+++ lib/main.dart
@@ -10,84 +10,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tarefas'),
         ),
-        home: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.blue,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.red,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Color.fromARGB(255, 55, 75, 147),
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Color.fromARGB(255, 255, 240, 65),
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Color.fromARGB(255, 237, 29, 36),
-                  height: 50,
-                  width: 50,
-                ),
-              ],
-            ),
-            Container(
-              color: Colors.white,
-              height: 30,
-              width: 300,
-              child: Text(
-                'Hello Alan',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 12,
-                ),
-                textAlign: TextAlign.center,
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 140,
               ),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  print('Você apertou o botão');
-                },
-                child: Text('Aperte o botão!'))
-          ],
-        ));
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Text('Aprender Flutter'),
+                    ElevatedButton(
+                        onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
   }
 }
---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,75 @@
+// Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Stack(
+//               alignment: AlignmentDirectional.center,
+//               children: [
+//                 Container(
+//                   color: Colors.blue,
+//                   width: 100,
+//                   height: 100,
+//                 ),
+//                 Container(
+//                   color: Colors.red,
+//                   width: 50,
+//                   height: 50,
+//                 )
+//               ],
+//             ),
+//             Stack(
+//               alignment: AlignmentDirectional.center,
+//               children: [
+//                 Container(
+//                   color: Colors.red,
+//                   width: 100,
+//                   height: 100,
+//                 ),
+//                 Container(
+//                   color: Colors.blue,
+//                   width: 50,
+//                   height: 50,
+//                 )
+//               ],
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               children: [
+//                 Container(
+//                   color: Color.fromARGB(255, 55, 75, 147),
+//                   height: 50,
+//                   width: 50,
+//                 ),
+//                 Container(
+//                   color: Color.fromARGB(255, 255, 240, 65),
+//                   height: 50,
+//                   width: 50,
+//                 ),
+//                 Container(
+//                   color: Color.fromARGB(255, 237, 29, 36),
+//                   height: 50,
+//                   width: 50,
+//                 ),
+//               ],
+//             ),
+//             Container(
+//               color: Colors.white,
+//               height: 30,
+//               width: 300,
+//               child: Text(
+//                 'Hello Alan',
+//                 style: TextStyle(
+//                   color: Colors.black,
+//                   fontSize: 12,
+//                 ),
+//                 textAlign: TextAlign.center,
+//               ),
+//             ),
+//             ElevatedButton(
+//                 onPressed: () {
+//                   print('Você apertou o botão');
+//                 },
+//                 child: Text('Aperte o botão!'))
+//           ],
+//         )
