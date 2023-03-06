bdc9f3e44736fdb0e4cd593ad52821e2c3e74158
Antonio Alan <tonio.alan07@gmail.com>
2023-03-06 00:16:21 -0300

    Container de texto/botao e manipulação de cores ARGB

--- lib/main.dart
+++ lib/main.dart
@@ -53,22 +53,40 @@ class MyApp extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  color: Colors.deepPurple,
+                  color: Color.fromARGB(255, 55, 75, 147),
                   height: 50,
                   width: 50,
                 ),
                 Container(
-                  color: Colors.indigo,
+                  color: Color.fromARGB(255, 255, 240, 65),
                   height: 50,
                   width: 50,
                 ),
                 Container(
-                  color: Colors.lime,
+                  color: Color.fromARGB(255, 237, 29, 36),
                   height: 50,
                   width: 50,
                 ),
               ],
             ),
+            Container(
+              color: Colors.white,
+              height: 30,
+              width: 300,
+              child: Text(
+                'Hello Alan',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print('Você apertou o botão');
+                },
+                child: Text('Aperte o botão!'))
           ],
         ));
   }
