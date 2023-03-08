81a2a26c54b4c2119cf85b3633e9d089ae8333ab
Antonio Alan <tonio.alan07@gmail.com>
2023-03-08 09:56:04 -0300

    Ajuste no codigo da primeira tela

--- lib/minha_primeira_tela.dart
+++ lib/minha_primeira_tela.dart
@@ -1,75 +1,84 @@
-// Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             Stack(
-//               alignment: AlignmentDirectional.center,
-//               children: [
-//                 Container(
-//                   color: Colors.blue,
-//                   width: 100,
-//                   height: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.red,
-//                   width: 50,
-//                   height: 50,
-//                 )
-//               ],
-//             ),
-//             Stack(
-//               alignment: AlignmentDirectional.center,
-//               children: [
-//                 Container(
-//                   color: Colors.red,
-//                   width: 100,
-//                   height: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.blue,
-//                   width: 50,
-//                   height: 50,
-//                 )
-//               ],
-//             ),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 Container(
-//                   color: Color.fromARGB(255, 55, 75, 147),
-//                   height: 50,
-//                   width: 50,
-//                 ),
-//                 Container(
-//                   color: Color.fromARGB(255, 255, 240, 65),
-//                   height: 50,
-//                   width: 50,
-//                 ),
-//                 Container(
-//                   color: Color.fromARGB(255, 237, 29, 36),
-//                   height: 50,
-//                   width: 50,
-//                 ),
-//               ],
-//             ),
-//             Container(
-//               color: Colors.white,
-//               height: 30,
-//               width: 300,
-//               child: Text(
-//                 'Hello Alan',
-//                 style: TextStyle(
-//                   color: Colors.black,
-//                   fontSize: 12,
-//                 ),
-//                 textAlign: TextAlign.center,
-//               ),
-//             ),
-//             ElevatedButton(
-//                 onPressed: () {
-//                   print('Você apertou o botão');
-//                 },
-//                 child: Text('Aperte o botão!'))
-//           ],
-//         )
+import 'package:flutter/material.dart';
+
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+            ),
+            Container(
+              color: Colors.red,
+              width: 50,
+              height: 50,
+            )
+          ],
+        ),
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+            ),
+            Container(
+              color: Colors.blue,
+              width: 50,
+              height: 50,
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              color: Color.fromARGB(255, 55, 75, 147),
+              height: 50,
+              width: 50,
+            ),
+            Container(
+              color: Color.fromARGB(255, 255, 240, 65),
+              height: 50,
+              width: 50,
+            ),
+            Container(
+              color: Color.fromARGB(255, 237, 29, 36),
+              height: 50,
+              width: 50,
+            ),
+          ],
+        ),
+        Container(
+          color: Colors.white,
+          height: 30,
+          width: 300,
+          child: Text(
+            'Hello Alan',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 12,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              print('Você apertou o botão');
+            },
+            child: Text('Aperte o botão!'))
+      ],
+    );
+  }
+}
