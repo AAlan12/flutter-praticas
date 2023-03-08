df4ffc1425e79096b43a78a603a3c2d301a8b153
Antonio Alan <tonio.alan07@gmail.com>
2023-03-08 10:51:20 -0300

    refatoração task e tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'difficulty.dart';
+import 'initial_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool opacidade = true;
+  
 
   @override
   Widget build(BuildContext context) {
@@ -22,182 +22,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Container(),
-          title: Text('Tarefas'),
-        ),
-        body: AnimatedOpacity(
-          opacity: opacidade ? 1 : 0,
-          duration: Duration(milliseconds: 800),
-          child: ListView(children: [
-            Task(
-                'Aprender Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                5),
-            Task(
-                'Finalizar FF8',
-                'https://upload.wikimedia.org/wikipedia/pt/thumb/5/58/FFVIII.jpg/300px-FFVIII.jpg',
-                4),
-            Task(
-                'Death Stranding',
-                'https://cdn1.epicgames.com/offer/0a9e3c5ab6684506bd624a849ca0cf39/EGS_DeathStrandingDirectorsCut_KOJIMAPRODUCTIONS_S4_1200x1600-5f99e16507795f9b497716b470cfd876',
-                2),
-            Task(
-                'Terminar Pequeno Príncipe',
-                'https://edipro.com.br/wp-content/uploads/2020/09/Pequeno-principe-O-grande-9788567097107-600x863.jpg',
-                1),
-            Task(
-                'Inglês',
-                'https://play-lh.googleusercontent.com/W7ati37w9D9qOPw4KXiRmWd3MNvW3mUFMcxRrDqCU9mq6FmQA0HyvYXTQiRLShASca8g',
-                2),
-            Task(
-                'Treinar a jogar de Jarvan',
-                'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/JarvanIV_30.jpg',
-                5),
-            Task(
-                'Academia',
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJpc4cT9ij2jBsTiVXjwCw-cAOoxn1ceGC6A&usqp=CAU',
-                3),
-          ]),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              opacidade = !opacidade;
-            });
-          },
-          child: Icon(Icons.remove_red_eye),
-        ),
-      ),
-    );
-  }
-}
-
-class Task extends StatefulWidget {
-  final String nome;
-  final String img;
-  final int dificuldade;
-
-  const Task(this.nome, this.img, this.dificuldade, {Key? key})
-      : super(key: key);
-
-  @override
-  State<Task> createState() => _TaskState();
-}
-
-class _TaskState extends State<Task> {
-  int nivel = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                color: Colors.blue,
-              ),
-              height: 140,
-            ),
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: Colors.white,
-                  ),
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          color: Colors.black26,
-                        ),
-                        width: 72,
-                        height: 100,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(4),
-                          child: Image.network(
-                            widget.img,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 200,
-                            child: Text(
-                              widget.nome,
-                              style: TextStyle(
-                                fontSize: 24,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ),
-                          Difficulty(difficultLevel: widget.dificuldade),
-                        ],
-                      ),
-                      Container(
-                        height: 52,
-                        width: 52,
-                        child: ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                nivel++;
-                              });
-                            },
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Icon(Icons.arrow_drop_up),
-                                Text(
-                                  'UP',
-                                  style: TextStyle(fontSize: 12),
-                                )
-                              ],
-                            )),
-                      )
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Container(
-                        child: LinearProgressIndicator(
-                          color: Colors.white,
-                          value: (widget.dificuldade > 0)
-                              ? (nivel / widget.dificuldade) / 10
-                              : 1,
-                        ),
-                        width: 200,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(12),
-                      child: Text(
-                        'Nivel $nivel',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            )
-          ],
-        ),
-      ),
+      home: InitialScreen(),
     );
   }
 }
